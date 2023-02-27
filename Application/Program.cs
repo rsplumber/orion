6d311c7eda1bf20d9a098a4b2a3a327414c0b363
s.kalami <s.kalami@tes.ir>
2023-02-27 10:14:11 +0330

    init : project skeleton.

---
+++ Application/Program.cs
@@ -0,0 +1,43 @@
+using FastEndpoints;
+using FastEndpoints.Swagger;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseKestrel();
+builder.WebHost.UseUrls("http://+:5162");
+
+builder.Services.AddCors();
+
+builder.Services.AddFastEndpoints();
+builder.Services.AddSwaggerDoc(settings =>
+{
+    settings.Title = "ObjectStorage - WebApi";
+    settings.DocumentName = "v1";
+    settings.Version = "v1";
+}, addJWTBearerAuth: false, maxEndpointVersion: 1);
+
+
+var app = builder.Build();
+
+
+app.UseCors(b => b.AllowAnyHeader()
+    .AllowAnyMethod()
+    .SetIsOriginAllowed(_ => true)
+    .AllowCredentials());
+
+app.UseFastEndpoints(config =>
+{
+    config.Endpoints.RoutePrefix = "api";
+    config.Versioning.Prefix = "v";
+    config.Versioning.PrependToRoute = true;
+    config.Endpoints.Filter = ep => ep.EndpointTags?.Contains("hidden") is not true;
+});
+
+
+// if (app.Environment.IsDevelopment())
+// {
+app.UseOpenApi();
+app.UseSwaggerUi3(s => s.ConfigureDefaults());
+// }
+
+
+await app.RunAsync();
