fa5bac9333f394cf9f2320c02479629fd5964481
s.kalami <s.kalami@tes.ir>
2023-04-25 09:22:18 +0330

    feature : ip server located.

--- Providers/MinIO.Storage/ServiceCollectionExtension.cs
+++ Providers/MinIO.Storage/ServiceCollectionExtension.cs
@@ -11,8 +11,8 @@ public static class ServiceCollectionExtension
     public static void AddMinio(this IServiceCollection services, IConfiguration configuration)
     {
         services.TryAddScoped<MinioClient>(provider => new MinioClient()
-            .WithEndpoint("192.168.70.117:9000")
-            .WithCredentials("EvZWw7VSbGCm4M9D", "5RQNFh5SI2NVxWiAxVp9VK5tvmWQ7BMN")
+            .WithEndpoint("10.121.254.62:9100")
+            .WithCredentials("oSeAMoNsIVEndENtLESa", "AURnMAyMUckbaFtHEreveRanTECTiM")
             .Build());
         services.TryAddScoped<IStorageService, StorageService>();
     }
